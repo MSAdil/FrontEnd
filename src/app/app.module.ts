521a980b5e8412af291f8ac8de52cc00be3d48f1
Mohd Shareef Adil <shareefadil73@gmail.com>
2020-06-09 18:32:05 +0200

    Angular Component part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,29 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import 'hammerjs';
+
 // after importing here the above modules needs to be defined in the imports in the 
 // NgModule decorator for the app module
 
+
+// automatically updated when u create a component
+import { MenuComponent } from './menu/menu.component';
+
+// import the MatList module here 
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent // components are added in declaration
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, // Modules added here
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
