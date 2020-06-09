1b6deee0a0d38e9cce962db976b3c28444d2c50f
Mohd Shareef Adil <shareefadil73@gmail.com>
2020-06-09 23:22:40 +0200

    Angular Component 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,6 +3,52 @@ import { Component, OnInit } from '@angular/core';
 // import the created Dish class here, so that we can use it as a type for a variable
 import { Dish } from '../shared/dish'; // then go MenuComponent class and declare a variable of the type Dish
 
+
+// cut the dishes from the class and declare a new constant here, LATER WE WILL REMOVE IT FROM HERE AND DECLARE IT AS A SERVICE
+const DISHES: Dish[] = [
+  {
+    id: '0',
+    name: 'Uthappizza',
+    image: '/assets/images/uthappizza.png',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '4.99',
+    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+  },
+  {
+    id: '1',
+    name: 'Zucchipakoda',
+    image: '/assets/images/zucchipakoda.png',
+    category: 'appetizer',
+    featured: false,
+    label: '',
+    price: '1.99',
+    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+  },
+  {
+    id: '2',
+    name: 'Vadonut',
+    image: '/assets/images/vadonut.png',
+    category: 'appetizer',
+    featured: false,
+    label: 'New',
+    price: '1.99',
+    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+  },
+  {
+    id: '3',
+    name: 'ElaiCheese Cake',
+    image: '/assets/images/elaicheesecake.png',
+    category: 'dessert',
+    featured: false,
+    label: '',
+    price: '2.99',
+    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+  }
+
+];
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -10,48 +56,9 @@ import { Dish } from '../shared/dish'; // then go MenuComponent class and declar
 })
 export class MenuComponent implements OnInit {
   // new variable dishes is declared here of the type Dish array
-  dishes : Dish[] = [
-    {
-      id: '0',
-      name: 'Uthappizza',
-      image: '/assets/images/uthappizza.png',
-      category: 'mains',
-      featured: true,
-      label: 'Hot',
-      price: '4.99',
-      description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-    },
-    {
-      id: '1',
-      name: 'Zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      featured: false,
-      label: '',
-      price: '1.99',
-      description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-    },
-    {
-      id: '2',
-      name: 'Vadonut',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer',
-      featured: false,
-      label: 'New',
-      price: '1.99',
-      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-    },
-    {
-      id: '3',
-      name: 'ElaiCheese Cake',
-      image: '/assets/images/elaicheesecake.png',
-      category: 'dessert',
-      featured: false,
-      label: '',
-      price: '2.99',
-      description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-    }
-   ];
+  dishes = DISHES;
+  // here we introduce one more variable called selectedDish, which is the first dish in an array
+  selectedDish: Dish = DISHES[0];
 
   constructor() { }
 
